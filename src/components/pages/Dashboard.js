bd3557bc21ec352ca40b9fcc6bf67891735b4ca6
Kwadwo Pobi Aning <aningabankwa@gmail.com>
2024-03-25 23:26:05 +0000

    fixed responsiveness

--- src/components/pages/Dashboard.js
+++ src/components/pages/Dashboard.js
@@ -4,19 +4,20 @@ import Header from '../Header';
 import styles from '../styles/dashboard.module.css'
 import image from '../assets/images/Rectangle139.png'
 import Logincomponents from '../Logincomponents';
-import { Button } from 'react-bootstrap';
+import Button from '../Button.js';
 import { Link, NavLink } from 'react-router-dom';
 import { Image } from 'react-bootstrap';
 import logo from '../assets/images/logo.png'
 import image2 from '../assets/images/Rectangle129.png'
 import '../styles/tabs.css'
+import image3 from '../assets/images/group12.png'
 
 
 const links = ["home", "about", "features", "pricing",];
 
 function Dashboard() {
   const [activeTab, setActiveTab] = useState('Featured');
-  const data = ['hello','hi','byebye']
+  const data = ['Introductory Lesson','hi','byebye']
   let num_cards = data.length
 
   const handleTabClick = (tabName) => {
@@ -24,33 +25,37 @@ function Dashboard() {
   };
   return(
     <div>
-       <div className={styles.topcontent} >
+      <div className={styles.topcontent} >
         
         <div className={styles.topdecoration}>
+
         <header className={styles.header}>
-      <Image className={styles.logo} src={logo} alt="logo" width={200} height={20} />
-      <nav className={styles.nav}>
-        <ul>
-          {links.map((link, index) => (
-            <Link key={index} to={`#${link}`} className={styles.link}>
-              <li className={`${styles.capitalize} ${styles.hoverTextWhite}`}>{link}</li>
-            </Link>
-          ))}
-        </ul>
-      </nav>
+          <Image className={styles.logo} src={logo} alt="logo" width={200} height={20} />
+          <nav className={styles.nav}>
+            <ul>
+              {links.map((link, index) => (
+                <Link key={index} to={`#${link}`} className={styles.link}>
+                  <li className={`${styles.capitalize} ${styles.hoverTextWhite}`}>{link}</li>
+                </Link>
+              ))}
+            </ul>
+          </nav>
     </header>
+    
   <div className={styles.subcontent}>
       <div className={styles.leftcontent}>
         <img className={styles.img} src={image}></img>
+        {/* <Image src={image2} width="25%" height='50%'></Image> */}
       </div>
+
       <div className={styles.rightcontent}>
-        <Image src={image2}></Image>
+        <Image src={image2} width="25%" height='50%'></Image>
         <h1>Hello Michael</h1>
         <p>Welcome to your Dashboard</p>
-        <div className={styles.test}>hello ghana
-          <div>activity</div>
-          <div>streak</div>
-          <div>achievement</div>
+        <div className={styles.test}>
+          <div className={styles.activitybox}></div>
+          <div className={styles.activitybox}></div>
+          <div className={styles.activitybox}></div>
         </div>
 
     </div>
@@ -80,8 +85,15 @@ function Dashboard() {
                     {data.map((item, index) => (
                       <div className={styles.lessoncontainer}>
                           <div className={styles.lessoncard} key={index}>
-                            <div className={styles.lessoncardleft}>{item}</div>
-                            <div className={styles.lessoncardright}> hhhh</div>
+                            <div className={styles.lessoncardleft}>
+                           
+                              
+                              <div className={styles.title}><h2>{item} </h2>Introdction to Coding in Robolabs</div>
+                              <div>Percentage complete</div>
+                              <Button>GO TO COURSE</Button>
+                            </div>
+                           
+                            <div className={styles.lessoncardright}> <Image src={image3} width="80%" height='100%' style={{ position: 'relative', top: '35%', transform: 'translateX(35%)' }}></Image></div>
                           
                           </div>
                       </div>
